d675670a2048597e2993cd22d35b79555776d834
gitcrumbs <ashwanisingh034@gmail.com>
2023-03-09 00:30:58 +0530

    Compilation fixes and Launch UI

--- BookMyConsultationUI/src/index.js
+++ BookMyConsultationUI/src/index.js
@@ -3,10 +3,24 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import Controller from "./screens/Controller";
+import {LoadingView} from "./component/LoadingView";
+import {Provider} from "react-redux";
+import {PersistGate} from 'redux-persist/integration/react';
+import {persistor, store} from './config/store';
+import {BrowserRouter as Router} from "react-router-dom";
 
-ReactDOM.render(<Controller />, document.getElementById("root"));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <PersistGate loading={<LoadingView/>} persistor={persistor}>
+            <Router>
+                <Controller/>
+            </Router>
+        </PersistGate>
+    </Provider>, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
